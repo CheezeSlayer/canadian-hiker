0f384af750bab26b516dc6e21f7990ddc1678dfa
CheezeSlayer <hchang1994@hotmail.com>
2019-01-02 15:07:35 -0800

    Added Portfolio Images

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const express = require('express');
 
 const app = express();
 
-const server = app.listen(3000, () => {
-  console.log(`Express running -> PORT ${server.address().port}`);
-});
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/public/view');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendfile('./views/index.html', {
+  res.sendfile('pages/home.html', {
   });
+
 });
 
 app.get('/pages/about.html', (req, res) => {
-  res.sendfile('./pages/about.html', {
+  res.sendfile('pages/about.html', {
   });
+
+});
+
+app.get('/pages/home.html', (req, res) => {
+  res.sendfile('pages/home.html', {
+  });
+
+});
+
+const server = app.listen(3000, () => {
+  console.log(`Express running -> PORT ${server.address().port}`);
 });
