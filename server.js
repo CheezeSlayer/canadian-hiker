1d3906edb88d8b6ddd97709aede4fde41f7463e4
CheezeSlayer <hchang1994@hotmail.com>
2019-01-06 16:49:53 -0800

    added email feature

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const nodeMailer = require('nodemailer');
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/public/view');
+//app.set('views', __dirname + '/public/view');
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 //--Define routes
 app.get('/', (req, res) => {
@@ -38,6 +40,37 @@ app.get('/pages/contact.html', (req, res) => {
   });
 
 });
+
+app.post('/send-email', function(req, res) {
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secureConnection: false,
+    auth: {
+        user: 'hchang1994@hotmail.com',
+        pass: 'hc6045998616'
+    },
+    tls: {
+      ciphers: 'SSLv3'
+    }
+  });
+  let mailOptions = {
+    from: `"Canadian Hiker" <hchang1994@hotmail.com>`,
+    to: 'hchang1994@hotmail.com',
+    subject: `Message From: ${req.body.fname} ${req.body.lname} (${req.body.email}) --- ${req.body.subject}`,
+    text: `${req.body.msg}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s send: %s', info.messageId, info.response);
+    res.sendfile('pages/contact.html', {
+    });
+  });
+});
+
 
 const server = app.listen(3000, () => {
   console.log(`Express running -> PORT ${server.address().port}`);
