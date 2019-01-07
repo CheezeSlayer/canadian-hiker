39755f532b11a8b09a0ae34eef49b84de0704026
CheezeSlayer <hchang1994@hotmail.com>
2019-01-06 19:21:14 -0800

    heroku images not loading

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('views', __dirname + '/public/view');
 
 app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
