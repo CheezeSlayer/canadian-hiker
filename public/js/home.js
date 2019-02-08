8e79ba3b1b1c4707c5af7bb7f6d5507ebe9090ca
CheezeSlayer <hchang1994@hotmail.com>
2019-02-07 18:41:01 -0800

    improved canvas load time

--- public/js/home.js
+++ public/js/home.js
@@ -10,43 +10,73 @@ var imgZoom;
 var arr;
 
 function loadCanvas(canvasJSON) {
-  arr = [];
-  $.getJSON(`/json/${canvasJSON}.json`, function(data) {
-    $.each(data.hikes, function(index, value) {
-      arr.push(data.hikes[index].imgSrc);
+  return new Promise(function(resolve, reject) {
+    arr = [];
+    $.getJSON(`/json/${canvasJSON}.json`, function(data) {
+      if(data != null && data != undefined)
+      {
+        $.each(data.hikes, function(index, value) {
+          arr.push(data.hikes[index].imgSrc);
+        });
+        resolve("Canvas Loaded");
+      } else {
+        reject(Error("No content to load"));
+      }
+      //console.log(arr.length);
+      //console.log(arr);
     });
-    //console.log(arr.length);
-    //console.log(arr);
-  });
+  })
+
 }
 
 function setCanvas(canvasID) {
-  setTimeout( function() {
-
+  return new Promise(function(resolve, reject) {
     modal = document.getElementById("imgModal");
     imgZoom = document.getElementsByClassName("imgModal-content")[0];
     console.log(arr.length);
-    for ( i = 0; i < arr.length; i++ ) {
-      var newImg = document.createElement("IMG");
-      newImg.setAttribute("src", `/images/portfolio/${canvasID}/${arr[i]}`);
-      newImg.setAttribute("width", "600");
-      newImg.setAttribute("height", "auto");
-      newImg.setAttribute("id", arr[i]);
-      console.log(newImg.src);
-      document.getElementById("imgCanvas").appendChild(newImg);
-      newImg.onclick = function() {
-        modal.style.display = "block";
-        imgZoom.src = this.src;
+    if(arr.length > 0){
+      for ( i = 0; i < arr.length; i++ ) {
+        var newImg = document.createElement("IMG");
+        newImg.setAttribute("src", `/images/portfolio/${canvasID}/${arr[i]}`);
+        newImg.setAttribute("width", "600");
+        newImg.setAttribute("height", "auto");
+        newImg.setAttribute("id", arr[i]);
+        console.log(newImg.src);
+        document.getElementById("imgCanvas").appendChild(newImg);
+        newImg.onclick = function() {
+          modal.style.display = "block";
+          imgZoom.src = this.src;
+        }
       }
+      resolve("Canvas Set");
+    } else {
+      reject("No canvas to set");
     }
-  }, 300);
+  })
 }
 
 function extZoom() {
   modal.style.display = "none";
 }
 
-function openCanvas() {
+function openCanvas(canvasName) {
+  loadCanvas(canvasName)
+  .then(function(value){
+    console.log("Loading Canvas:...");
+    console.log("Status: ", value);
+    return(setCanvas(canvasName))
+  })
+  .then(function(value){
+    console.log("Setting Canvas:...");
+    console.log("Status: ", value);
+  })
+  .then(function(){
+    return true;
+  })
+  .catch(function(error){
+    console.log(error);
+    return false;
+  })
   document.getElementById("overlay").style.width = "100%";
 }
 
